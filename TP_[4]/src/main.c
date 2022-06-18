8cc9278ebf8a3d4a7dbe552a69c3f02d3eb86f52
More Escudero <moreescudero@Air-de-More.fibertel.com.ar>
2022-06-18 20:28:38 -0300

    TP 4

---
+++ TP_[4]/src/main.c
@@ -0,0 +1,83 @@
+/*
+    utest example : Unit test examples.
+    Copyright (C) <2018>  <Mauricio Davila>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../testing/inc/main_test.h"
+#include "../inc/LinkedList.h"
+
+
+int main(void)
+{
+	//startTesting(1);  // ll_newLinkedList				//100
+	//startTesting(2);  // ll_len							//100
+	//startTesting(3);  // getNode - test_getNode			//100
+	//startTesting(4);  // addNode - test_addNode			//100
+	//startTesting(5);  // ll_add							//100
+	//startTesting(6);  // ll_get							//100
+	//startTesting(7);  // ll_set							//100
+	//startTesting(8);  // ll_remove						//100
+	//startTesting(9);  // ll_clear						//100
+	//startTesting(10); // ll_deleteLinkedList			//100
+	//startTesting(11); // ll_indexOf						//100
+	//startTesting(12); // ll_isEmpty						//100
+	//startTesting(13); // ll_push						//100
+	//startTesting(14); // ll_pop							//100
+	//startTesting(15); // ll_contains					//100
+	//startTesting(16); // ll_containsAll					//100
+	//startTesting(17); // ll_subList						//100
+	//startTesting(18); // ll_clone						//100
+	//startTesting(19); // ll_sort 						//
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
